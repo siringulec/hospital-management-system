812aad62867932fd556de8a17230a3f6e259a97e
Şirin Güleç <siringulec@192.168.1.35>
2023-01-06 23:31:34 +0300

    update database

--- hospital.sql
+++ hospital.sql
@@ -3,265 +3,210 @@ SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `appointments`;
 
 CREATE TABLE `appointments` (
-  `appointmentID` int(11) unsigned NOT NULL,
-  `patientID` int(11) unsigned NOT NULL,
-  `doctorID` int(11) unsigned NOT NULL,
-  `date` datetime NOT NULL,
+  `appointmentID` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `patientID` bigint(11) unsigned NOT NULL,
+  `doctorID` bigint(11) unsigned NOT NULL,
+  `date` date NOT NULL,
+  `time` time DEFAULT NULL,
   PRIMARY KEY (`appointmentID`),
   KEY `patientID` (`patientID`),
   KEY `doctorID` (`doctorID`),
-  CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`),
-  CONSTRAINT `appointments_ibfk_3` FOREIGN KEY (`doctorID`) REFERENCES `doctor` (`identification_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`doctorID`) REFERENCES `doctor` (`identification_number`) ON DELETE CASCADE,
+  CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `appointments` WRITE;
 
-
-INSERT INTO `appointments` (`appointmentID`, `patientID`, `doctorID`, `date`)
+INSERT INTO `appointments` (`appointmentID`, `patientID`, `doctorID`, `date`, `time`)
 VALUES
-	(1,1,1,'2022-11-09 09:00:00'),
-	(2,2,3,'2022-11-09 09:30:00'),
-	(3,3,2,'2022-11-09 09:00:00');
-
+	(1,1,1,'2023-01-02','14:45:00'),
+	(3,3,2,'2022-11-09','09:00:00'),
+	(4,1,1,'2023-03-04','09:45:00');
 
 UNLOCK TABLES;
-
 
 DROP TABLE IF EXISTS `bills`;
 
 CREATE TABLE `bills` (
-  `bill_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `patientID` int(11) unsigned NOT NULL,
-  `received_treatment` text DEFAULT NULL,
-  `basic_amount` double DEFAULT NULL,
-  `amount_assured` double DEFAULT NULL,
-  `bill_status` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`bill_id`),
+  `patientID` bigint(11) unsigned NOT NULL,
+  `basic_amount` double NOT NULL,
+  `amount_assured` double NOT NULL,
+  `date` date NOT NULL,
   KEY `patientID` (`patientID`),
-  CONSTRAINT `bills_ibfk_1` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  CONSTRAINT `bills_ibfk_2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `bills` WRITE;
 
-
-INSERT INTO `bills` (`bill_id`, `patientID`, `received_treatment`, `basic_amount`, `amount_assured`, `bill_status`)
+INSERT INTO `bills` (`patientID`, `basic_amount`, `amount_assured`, `date`)
 VALUES
-	(1,2,'Send home for rest with prescripted',200,40,'Paid'),
-	(2,1,'Still undergoing treatment ',NULL,NULL,'Pending'),
-	(3,3,'Still undergoing treatment ',NULL,NULL,'Pending');
-
+	(3,100,0,'2023-01-02'),
+	(2,200,100,'2023-01-02');
 
 UNLOCK TABLES;
-
-
 
 DROP TABLE IF EXISTS `diagnosis`;
 
 CREATE TABLE `diagnosis` (
-  `diagnosisID` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `patientID` int(11) unsigned NOT NULL,
-  `testID` int(11) unsigned DEFAULT NULL,
-  `diagnosis` text DEFAULT NULL,
-  `status` varchar(10) NOT NULL,
-  `prescriptionID` int(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`diagnosisID`),
+  `patientID` bigint(11) unsigned NOT NULL,
+  `symptoms` text NOT NULL,
+  `diagnosis` varchar(225) NOT NULL,
+  `date` date NOT NULL,
   KEY `patientID` (`patientID`),
-  KEY `testID` (`testID`),
-  KEY `prescriptionID` (`prescriptionID`),
-  CONSTRAINT `diagnosis_ibfk_1` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`),
-  CONSTRAINT `diagnosis_ibfk_2` FOREIGN KEY (`testID`) REFERENCES `test` (`testID`),
-  CONSTRAINT `diagnosis_ibfk_3` FOREIGN KEY (`prescriptionID`) REFERENCES `prescription` (`prescriptionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-LOCK TABLES `diagnosis` WRITE;
-
-
-INSERT INTO `diagnosis` (`diagnosisID`, `patientID`, `testID`, `diagnosis`, `status`, `prescriptionID`)
-VALUES
-	(1,1,1,'Diabetes','finalized',1),
-	(2,2,2,NULL,'pending',NULL),
-	(3,3,3,'Acinetobacter Infection','finalized',3);
-
-
-UNLOCK TABLES;
-
+  CONSTRAINT `diagnosis_ibfk_2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 DROP TABLE IF EXISTS `doctor`;
 
 CREATE TABLE `doctor` (
-  `identification_number` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `identification_number` bigint(11) unsigned NOT NULL,
   `name` varchar(100) NOT NULL,
-  `subspecialty` varchar(50) DEFAULT NULL,
   `pass` varchar(12) NOT NULL,
-  `age` int(3) DEFAULT NULL,
-  `gender` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `address` text DEFAULT NULL,
-  `phone` varchar(11) DEFAULT NULL,
-  PRIMARY KEY (`identification_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `subspecialty` varchar(50) NOT NULL,
+  `gender` enum('Female','Male','Other') NOT NULL,
+  `phone` varchar(25) NOT NULL,
+  PRIMARY KEY (`identification_number`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `doctor` WRITE;
 
-
-INSERT INTO `doctor` (`identification_number`, `name`, `subspecialty`, `pass`, `age`, `gender`, `address`, `phone`)
+INSERT INTO `doctor` (`identification_number`, `name`, `pass`, `subspecialty`, `gender`, `phone`)
 VALUES
-	(1,'Michael Joseph Giordano','Neurological Surgeon','1',52,X'4D','263 Farmington Ave  Farmington/Hartford','1'),
-	(2,'Lee Ann Van Houten-Sauter','Family Doctor','lavhs',34,X'46','220 Pine St Williamstown/Gloucester','3'),
-	(3,'Glenn Howard Perelson','Cardiologist','ghp',43,X'4D','740 Bay Blvd Chula Vista/San Diego','4');
+	(1,'Michael Joseph Giordano','1','Neurological Surgeon','Male','1'),
+	(2,'Lee Ann Van Houten-Sauter','lavhs','Family Doctor','Female','3'),
+	(3,'Glenn Howard Perelson','ghp','Cardiologist','Male','4'),
+	(12345678911,'Mike Drop','mike123','Dermatologist','Male','+905324567865'),
+	(21161191122,'Mimi Stantez Gordino','LoveMeMimi','Family Doctor','Other','+18398723081');
 
 
 UNLOCK TABLES;
-
-
-
 
 DROP TABLE IF EXISTS `medicine`;
 
 CREATE TABLE `medicine` (
   `medicineID` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `stock` int(225) DEFAULT NULL,
+  `stock` int(225) unsigned DEFAULT NULL,
   PRIMARY KEY (`medicineID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `medicine` WRITE;
-
 
 INSERT INTO `medicine` (`medicineID`, `name`, `stock`)
 VALUES
 	(1,'Metformin',65),
 	(2,'Bactepan',23),
-	(3,'Lysotrope Ethozoxane',14);
-
+	(3,'Lysotrope Ethozoxane',14),
+	(4,'Magvital',25);
 
 UNLOCK TABLES;
-
-
-
 
 DROP TABLE IF EXISTS `patient`;
 
 CREATE TABLE `patient` (
-  `identification_number` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `identification_number` bigint(11) unsigned NOT NULL,
   `name` varchar(100) NOT NULL,
-  `password` varchar(12) NOT NULL,
-  `age` int(3) DEFAULT NULL,
-  `gender` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `phone` varchar(11) NOT NULL,
+  `pass` varchar(12) NOT NULL,
+  `age` int(3) unsigned NOT NULL,
+  `gender` enum('Female','Male','Other') NOT NULL,
+  `phone` varchar(25) NOT NULL,
   PRIMARY KEY (`identification_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `patient` WRITE;
 
-
-INSERT INTO `patient` (`identification_number`, `name`, `password`, `age`, `gender`, `phone`)
+INSERT INTO `patient` (`identification_number`, `name`, `pass`, `age`, `gender`, `phone`)
 VALUES
-	(1,'Mabel Mcgregor','i3ejr2ooujfo',18,X'46','1'),
-	(2,'Nicholas Cervantes','wklemdfw',61,X'4D','1'),
-	(3,'Adelina Nairn','61771991',41,X'46','1');
+	(1,'test','test',40,'Male','+904325009020'),
+	(2,'testing','testing',50,'Other','+354424143413'),
+	(3,'Adelina Nairn','61771991',41,'Female','1'),
+	(12345678900,'Jake Tormend','2',23,'Male','+905324567742'),
+	(12475408654,'Ziggi Zaggi','zigzag',54,'Other','+907358367837'),
+	(22222222222,'Light Saber','111',22,'Male','22222222222'),
+	(53076288498,'Fatih Kaza','demo',32,'Male','+905307628849'),
+	(63870583195,'sarah dienta','dinadina',12,'Female','+63870583195'),
+	(65914025136,'Sirin Gulec','sirin',20,'Female','+905441320016'),
+	(98147918741,'Kelsey Candy','nice',24,'Female','+905237652343');
 
 UNLOCK TABLES;
-
-
 
 DROP TABLE IF EXISTS `prescription`;
 
 CREATE TABLE `prescription` (
-  `prescriptionID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `patientID` bigint(11) unsigned NOT NULL,
   `medicineID` int(11) unsigned NOT NULL,
   `dosage` varchar(225) NOT NULL,
-  PRIMARY KEY (`prescriptionID`),
+  `date` date NOT NULL,
   KEY `medicineID` (`medicineID`),
-  CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`medicineID`) REFERENCES `medicine` (`medicineID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `patientID` (`patientID`),
+  CONSTRAINT `prescription_ibfk_1` FOREIGN KEY (`medicineID`) REFERENCES `medicine` (`medicineID`),
+  CONSTRAINT `prescription_ibfk_2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `prescription` WRITE;
 
-
-INSERT INTO `prescription` (`prescriptionID`, `medicineID`, `dosage`)
+INSERT INTO `prescription` (`patientID`, `medicineID`, `dosage`, `date`)
 VALUES
-	(1,1,'500mg'),
-	(2,3,'750mg'),
-	(3,2,'250mg');
-
+	(3,3,'750mg','2022-11-14'),
+	(1,4,'500mg','2023-01-01'),
+	(53076288498,1,'500mg','2023-01-02'),
+	(1,1,'100mg','2022-12-02');
 
 UNLOCK TABLES;
-
-
-
-DROP TABLE IF EXISTS `room`;
-
-CREATE TABLE `room` (
-  `room_number` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `purpose` varchar(50) DEFAULT NULL,
-  `availability` tinytext DEFAULT NULL,
-  PRIMARY KEY (`room_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-LOCK TABLES `room` WRITE;
-
-
-INSERT INTO `room` (`room_number`, `purpose`, `availability`)
-VALUES
-	(1,'Operating Room','Y'),
-	(2,'Patient Room','N'),
-	(3,'Patient Room','Y'),
-	(4,'Emergency Room','N'),
-	(5,'Patient Room','Y');
-
-
-UNLOCK TABLES;
-
-
-
-DROP TABLE IF EXISTS `stay`;
-
-CREATE TABLE `stay` (
-  `room_number` int(11) unsigned NOT NULL,
-  `patientID` int(11) unsigned NOT NULL,
-  `reason` text DEFAULT NULL,
-  `date_entered` date DEFAULT NULL,
-  `recovery_status` text DEFAULT NULL,
-  `number_of_days` int(225) DEFAULT NULL,
-  KEY `room_number` (`room_number`),
-  KEY `patientID` (`patientID`),
-  CONSTRAINT `stay_ibfk_1` FOREIGN KEY (`room_number`) REFERENCES `room` (`room_number`),
-  CONSTRAINT `stay_ibfk_2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-LOCK TABLES `stay` WRITE;
-
-
-INSERT INTO `stay` (`room_number`, `patientID`, `reason`, `date_entered`, `recovery_status`, `number_of_days`)
-VALUES
-	(2,1,'High blood sugar','2022-11-09','Patient has shown improvement.',2),
-	(4,3,'Dangerous Infection','2022-11-09','Shows signs of possible expo',5);
-
-
-UNLOCK TABLES;
-
-
 
 DROP TABLE IF EXISTS `test`;
 
 CREATE TABLE `test` (
-  `testID` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `patientID` int(11) unsigned NOT NULL,
-  `type_of_test` varchar(50) DEFAULT NULL,
+  `patientID` bigint(11) unsigned NOT NULL,
+  `type_of_test` bigint(11) unsigned NOT NULL,
   `results` text DEFAULT NULL,
-  PRIMARY KEY (`testID`),
+  `date` date NOT NULL,
   KEY `patientID` (`patientID`),
-  CONSTRAINT `test_ibfk_2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `type_of_test` (`type_of_test`),
+  CONSTRAINT `test_ibfk_1` FOREIGN KEY (`patientID`) REFERENCES `patient` (`identification_number`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `test_ibfk_2` FOREIGN KEY (`type_of_test`) REFERENCES `type_of_test` (`typeID`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOCK TABLES `test` WRITE;
 
 
-INSERT INTO `test` (`testID`, `patientID`, `type_of_test`, `results`)
+INSERT INTO `test` (`patientID`, `type_of_test`, `results`, `date`)
 VALUES
-	(1,1,'Blood Test',NULL),
-	(2,2,'Urine Test',NULL),
-	(3,3,'X-ray',NULL);
+	(1,1,NULL,'2023-01-02'),
+	(2,5,NULL,'2023-01-02'),
+	(3,9,NULL,'2023-01-02'),
+	(3,9,NULL,'2023-01-02');
 
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `type_of_test`;
+
+CREATE TABLE `type_of_test` (
+  `typeID` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type_of_test` varchar(225) NOT NULL,
+  PRIMARY KEY (`typeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+LOCK TABLES `type_of_test` WRITE;
+
+INSERT INTO `type_of_test` (`typeID`, `type_of_test`)
+VALUES
+	(1,'X-ray'),
+	(2,'Concussion Test'),
+	(3,'MR'),
+	(4,'Ultrasound'),
+	(5,'Allergy Blood '),
+	(6,'Heart Disease Risk Assessment'),
+	(7,'Vitamin D'),
+	(8,'Yeast Infection'),
+	(9,'Hemoglobin'),
+	(10,'Glucose in Urine'),
+	(11,'Calcium in Blood'),
+	(12,'Blood Glucose'),
+	(13,'Appendicitis Tests'),
+	(14,'Pregnancy Test'),
+	(15,'Sleep Study'),
+	(16,'TSH ');
 
 UNLOCK TABLES;
